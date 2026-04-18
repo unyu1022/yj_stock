7caf94e94a2c1f44639a9b448084f1c458a51a75
unyu1022 <kyj921022@gmail.com>
2026-04-19 02:59:36 +0900

    Add Worker entrypoint

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { onRequestGet as onSearch } from "../functions/api/search.js";
+import { onRequestGet as onStock } from "../functions/api/stock.js";
+import { onRequestGet as onHealth } from "../functions/api/health.js";
+
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (request.method === "GET" && url.pathname === "/api/search") {
+      return onSearch({ request, env });
+    }
+
+    if (request.method === "GET" && url.pathname === "/api/stock") {
+      return onStock({ request, env });
+    }
+
+    if (request.method === "GET" && url.pathname === "/api/health") {
+      return onHealth({ request, env });
+    }
+
+    return env.ASSETS.fetch(request);
+  },
+};
