18f5004287067de5524cfb01eb69d4962269aa22
unyu1022 <kyj921022@gmail.com>
2026-04-19 19:11:11 +0900

    Add master API route to worker entry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { onRequestGet as onMaster } from "../functions/api/master.js";
 import { onRequestGet as onSearch } from "../functions/api/search.js";
 import { onRequestGet as onStock } from "../functions/api/stock.js";
 import { onRequestGet as onHealth } from "../functions/api/health.js";
@@ -8,6 +9,10 @@ export default {
 
     if (request.method === "GET" && url.pathname === "/api/search") {
       return onSearch({ request, env });
+    }
+
+    if (request.method === "GET" && url.pathname === "/api/master") {
+      return onMaster({ request, env });
     }
 
     if (request.method === "GET" && url.pathname === "/api/stock") {
